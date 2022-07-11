a1266fb1426ead2b56583ef7cca52d143017db80
ikuma.nagahara <ikuma.nagahara@pixelsoft.co.jp>
2022-07-11 16:00:43 +0900

    add SingleLinkedList and unittest

---
+++ linkedlist/SingleLinkedList.go
@@ -0,0 +1,163 @@
+package linkedlist
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	value int
+	next  *Node
+}
+
+type SingleLinkedList struct {
+	head *Node
+}
+
+func NewSingleLinkedList() *SingleLinkedList {
+	return &SingleLinkedList{
+		head: nil,
+	}
+}
+
+func (sl *SingleLinkedList) Append(appendValue int) {
+	var newNode *Node = &Node{value: appendValue, next: nil}
+
+	if sl.head == nil {
+		sl.head = newNode
+		return
+	}
+
+	current := sl.head
+	for {
+		if current.next == nil {
+			break
+		}
+		current = current.next
+	}
+	current.next = newNode
+}
+
+func (sl SingleLinkedList) Print() {
+	current := sl.head
+
+	for {
+		if current == nil {
+			break
+		}
+		fmt.Println(current.value)
+		current = current.next
+	}
+}
+
+func (sl *SingleLinkedList) Insert(insertValue int) {
+	var newNode *Node = &Node{value: insertValue, next: nil}
+
+	newNode.next = sl.head
+	sl.head = newNode
+}
+
+func (sl *SingleLinkedList) RemoveElement(target int) {
+	var current *Node = sl.head
+	var previous *Node = nil
+
+	for {
+		if current == nil {
+			break
+		}
+
+		if current.value == target {
+			if previous != nil {
+				previous.next = current.next
+			} else {
+				sl.head = current.next
+			}
+
+			return
+
+		} else {
+			previous = current
+			current = current.next
+		}
+	}
+}
+
+func (sl *SingleLinkedList) RemoveElements(target int) {
+	var current *Node = sl.head
+	var previous *Node = nil
+
+	for {
+		if current == nil {
+			break
+		}
+
+		if current.value == target {
+			if previous != nil {
+				previous.next = current.next
+			} else {
+				sl.head = current.next
+			}
+
+			current = current.next
+		} else {
+			previous = current
+			current = current.next
+		}
+	}
+}
+
+func (sl *SingleLinkedList) ReverseIterative() {
+	var previous *Node = nil
+	var current *Node = sl.head
+
+	for {
+		if current == nil {
+			break
+		}
+
+		nextNode := current.next
+		current.next = previous
+		previous = current
+		current = nextNode
+	}
+	sl.head = previous
+}
+
+func (sl *SingleLinkedList) ReverseRecursive() {
+	sl.head = reverseRecursive(sl.head, nil)
+}
+
+func reverseRecursive(current *Node, previous *Node) *Node {
+	if current == nil {
+		return previous
+	}
+
+	nextNode := current.next
+	current.next = previous
+	previous = current
+	current = nextNode
+
+	return reverseRecursive(current, previous)
+
+}
+
+func (sl *SingleLinkedList) SortMyself() {
+	var current *Node = sl.head
+
+	for {
+		if current == nil {
+			break
+		}
+
+		nextNode := current.next
+		for {
+			if nextNode == nil {
+				break
+			}
+			if current.value > nextNode.value {
+				current.value, nextNode.value = nextNode.value, current.value
+			}
+			nextNode = nextNode.next
+		}
+		current = current.next
+	}
+}
---
+++ linkedlist/SingleLinkedList_test.go
@@ -0,0 +1,206 @@
+package linkedlist
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLinkedList_Append(t *testing.T) {
+	// Init
+	l := NewSingleLinkedList()
+
+	// Execution
+	l.Append(5)
+	l.Append(9)
+	l.Append(2)
+	l.Append(4)
+
+	// Test
+	assert.Equal(t, 5, l.head.value)
+	assert.Equal(t, 9, l.head.next.value)
+	assert.Equal(t, 2, l.head.next.next.value)
+	assert.Equal(t, 4, l.head.next.next.next.value)
+
+	// watch values
+	l.Print()
+}
+
+func TestSingleLinkedList_Insert(t *testing.T) {
+	// Init
+	l := NewSingleLinkedList()
+	l.Append(0)
+	l.Append(1)
+	l.Append(2)
+	l.Append(3)
+
+	// Execution
+	l.Insert(4)
+
+	// Test
+	assert.Equal(t, 4, l.head.value)
+	assert.Equal(t, 0, l.head.next.value)
+	assert.Equal(t, 1, l.head.next.next.value)
+	assert.Equal(t, 2, l.head.next.next.next.value)
+	assert.Equal(t, 3, l.head.next.next.next.next.value)
+
+	// watch values
+	l.Print()
+}
+
+// 先頭を削除
+func TestSingleLinkedList_RemoveElement(t *testing.T) {
+	// Init
+	l := NewSingleLinkedList()
+	l.Append(0)
+	l.Append(1)
+	l.Append(2)
+	l.Append(3)
+
+	// Execution
+	l.RemoveElement(0)
+
+	// Test
+	assert.Equal(t, 1, l.head.value)
+	assert.Equal(t, 2, l.head.next.value)
+	assert.Equal(t, 3, l.head.next.next.value)
+
+	// watch values
+	l.Print()
+}
+
+// 真ん中を削除
+func TestSingleLinkedList_RemoveElement2(t *testing.T) {
+	// Init
+	l := NewSingleLinkedList()
+	l.Append(0)
+	l.Append(1)
+	l.Append(2)
+	l.Append(1)
+	l.Append(3)
+
+	// Execution
+	l.RemoveElement(1)
+
+	// Test
+	assert.Equal(t, 0, l.head.value)
+	assert.Equal(t, 2, l.head.next.value)
+	assert.Equal(t, 1, l.head.next.next.value)
+	assert.Equal(t, 3, l.head.next.next.next.value)
+
+	// watch values
+	l.Print()
+}
+
+// 最後尾を削除
+func TestSingleLinkedList_RemoveElement3(t *testing.T) {
+	// Init
+	l := NewSingleLinkedList()
+	l.Append(0)
+	l.Append(1)
+	l.Append(2)
+	l.Append(3)
+
+	// Execution
+	l.RemoveElement(3)
+
+	// Test
+	assert.Equal(t, 0, l.head.value)
+	assert.Equal(t, 1, l.head.next.value)
+	assert.Equal(t, 2, l.head.next.next.value)
+
+	// watch values
+	l.Print()
+}
+
+func TestSingleLinkedList_RemoveElements(t *testing.T) {
+	// Init
+	l := NewSingleLinkedList()
+	l.Append(1)
+	l.Append(1)
+	l.Append(0)
+	l.Append(1)
+	l.Append(2)
+	l.Append(1)
+	l.Append(1)
+	l.Append(3)
+	l.Append(1)
+	l.Append(1)
+
+	// Execution
+	l.RemoveElements(1)
+
+	// Test
+	assert.Equal(t, 0, l.head.value)
+	assert.Equal(t, 2, l.head.next.value)
+	assert.Equal(t, 3, l.head.next.next.value)
+
+	// watch values
+	l.Print()
+}
+
+func TestSingleLinkedList_Reverse(t *testing.T) {
+	// Init
+	l := NewSingleLinkedList()
+	l.Append(0)
+	l.Append(1)
+	l.Append(2)
+	l.Append(3)
+
+	// Execution
+	l.ReverseIterative()
+
+	// Test
+	assert.Equal(t, 3, l.head.value)
+	assert.Equal(t, 2, l.head.next.value)
+	assert.Equal(t, 1, l.head.next.next.value)
+	assert.Equal(t, 0, l.head.next.next.next.value)
+
+	// watch values
+	l.Print()
+}
+
+func TestSingleLinkedList_ReverseRecursive(t *testing.T) {
+	// Init
+	l := NewSingleLinkedList()
+	l.Append(0)
+	l.Append(1)
+	l.Append(2)
+	l.Append(3)
+
+	// Execution
+	l.ReverseRecursive()
+
+	// Test
+	assert.Equal(t, 3, l.head.value)
+	assert.Equal(t, 2, l.head.next.value)
+	assert.Equal(t, 1, l.head.next.next.value)
+	assert.Equal(t, 0, l.head.next.next.next.value)
+
+	// watch values
+	l.Print()
+}
+
+func TestSingleLinkedList_SortMyself(t *testing.T) {
+	// Init
+	l := NewSingleLinkedList()
+	l.Append(6)
+	l.Append(1)
+	l.Append(9)
+	l.Append(4)
+	l.Append(7)
+	l.Append(3)
+
+	// Execution
+	l.SortMyself()
+
+	// Test
+	assert.Equal(t, 1, l.head.value)
+	assert.Equal(t, 3, l.head.next.value)
+	assert.Equal(t, 4, l.head.next.next.value)
+	assert.Equal(t, 6, l.head.next.next.next.value)
+	assert.Equal(t, 7, l.head.next.next.next.next.value)
+	assert.Equal(t, 9, l.head.next.next.next.next.next.value)
+
+	// watch values
+	l.Print()
+}
