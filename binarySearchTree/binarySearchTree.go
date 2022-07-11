4d714d46751e33818abffca63aa8906167092d39
ikuma.nagahara <ikuma.nagahara@pixelsoft.co.jp>
2022-07-11 18:10:28 +0900

    add binary search tree

---
+++ binarySearchTree/binarySearchTree.go
@@ -0,0 +1,49 @@
+package binarySearchTree
+
+import "fmt"
+
+type Node struct {
+	value int
+	left  *Node
+	right *Node
+}
+
+type binarySearchTree struct {
+	root *Node
+}
+
+func NewBinarySearchTree() *binarySearchTree {
+	return &binarySearchTree{
+		root: nil,
+	}
+}
+
+func (bst *binarySearchTree) Insert(InsertValue int) {
+	bst.root = bst.insert(bst.root, InsertValue)
+}
+
+func (bst *binarySearchTree) insert(root *Node, insertValue int) *Node {
+	newNode := &Node{value: insertValue, left: nil, right: nil}
+	if root == nil {
+		return newNode
+	}
+
+	if root.value < insertValue {
+		root.right = bst.insert(root.right, insertValue)
+	} else if root.value > insertValue {
+		root.left = bst.insert(root.left, insertValue)
+	}
+	return root
+}
+
+func (bst *binarySearchTree) Inorder() {
+	bst.inorder(bst.root)
+}
+
+func (bst *binarySearchTree) inorder(node *Node) {
+	if node != nil {
+		bst.inorder(node.left)
+		fmt.Println(node.value)
+		bst.inorder(node.right)
+	}
+}
---
+++ binarySearchTree/binarySearchTree_test.go
@@ -0,0 +1,30 @@
+package binarySearchTree
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBinarySearchTree_Insert(t *testing.T) {
+	// Init
+	bst := NewBinarySearchTree()
+
+	// Execution
+	bst.Insert(6)
+	bst.Insert(4)
+	bst.Insert(9)
+	bst.Insert(7)
+	bst.Insert(2)
+	bst.Insert(10)
+
+	// Test
+	assert.Equal(t, 6, bst.root.value)
+	assert.Equal(t, 4, bst.root.left.value)
+	assert.Equal(t, 9, bst.root.right.value)
+	assert.Equal(t, 7, bst.root.right.left.value)
+	assert.Equal(t, 2, bst.root.left.left.value)
+	assert.Equal(t, 10, bst.root.right.right.value)
+
+	// Watch values
+	bst.Inorder()
+}
