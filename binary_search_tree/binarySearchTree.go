b5f298a02d0397cff00fb2b35deaab4f1b0b08b9
ikuma.nagahara <ikuma.nagahara@pixelsoft.co.jp>
2022-07-12 14:08:41 +0900

    update

--- binary_search_tree/binarySearchTree.go
+++ binary_search_tree/binarySearchTree.go
@@ -1,6 +1,9 @@
 package binary_search_tree
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Node struct {
 	value int
@@ -19,31 +22,186 @@ func NewBinarySearchTree() *binarySearchTree {
 }
 
 func (bst *binarySearchTree) Insert(InsertValue int) {
-	bst.root = bst.insert(bst.root, InsertValue)
+	bst.root = insert(bst.root, InsertValue)
 }
 
-func (bst *binarySearchTree) insert(root *Node, insertValue int) *Node {
+func insert(root *Node, insertValue int) *Node {
 	newNode := &Node{value: insertValue, left: nil, right: nil}
 	if root == nil {
 		return newNode
 	}
 
 	if root.value < insertValue {
-		root.right = bst.insert(root.right, insertValue)
+		root.right = insert(root.right, insertValue)
 	} else if root.value > insertValue {
-		root.left = bst.insert(root.left, insertValue)
+		root.left = insert(root.left, insertValue)
 	}
 	return root
 }
 
 func (bst *binarySearchTree) Inorder() {
-	bst.inorder(bst.root)
+	inorder(bst.root)
 }
 
-func (bst *binarySearchTree) inorder(node *Node) {
+func inorder(node *Node) {
 	if node != nil {
-		bst.inorder(node.left)
+		inorder(node.left)
 		fmt.Println(node.value)
-		bst.inorder(node.right)
+		inorder(node.right)
 	}
+}
+
+func (bst *binarySearchTree) Search(TargetValue int) bool {
+	return search(bst.root, TargetValue)
+}
+
+func search(node *Node, targetValue int) bool {
+	if node == nil {
+		return false
+	}
+
+	if node.value == targetValue {
+		return true
+	} else if node.value < targetValue {
+		return search(node.right, targetValue)
+	} else {
+		return search(node.left, targetValue)
+	}
+}
+
+func getMinimumNode(node *Node) *Node {
+	for {
+		if node.left == nil {
+			break
+		}
+
+		node = node.left
+	}
+	return node
+}
+
+func (bst *binarySearchTree) Remove(RemoveValue int) {
+	bst.root = remove(bst.root, RemoveValue)
+}
+
+func remove(node *Node, removeValue int) *Node {
+	if node == nil {
+		return node
+	}
+
+	if node.value < removeValue {
+		node.right = remove(node.right, removeValue)
+	} else if node.value > removeValue {
+		node.left = remove(node.left, removeValue)
+	} else {
+		if node.right == nil {
+			return node.left
+		} else if node.left == nil {
+			return node.right
+		}
+
+		temp := getMinimumNode(node.right)
+		node.value = temp.value
+		node.right = remove(node.right, temp.value)
+	}
+	return node
+}
+
+func (bst *binarySearchTree) MaxDepth() int {
+	return int(maxDepth(bst.root))
+}
+
+func maxDepth(node *Node) float64 {
+	if node == nil {
+		return 0
+	}
+
+	left := maxDepth(node.left)
+	right := maxDepth(node.right)
+
+	return math.Max(left, right) + 1
+
+}
+
+func (bst *binarySearchTree) PathSum(TargetSum int) bool {
+	return pathSum(bst.root, TargetSum)
+}
+
+func pathSum(node *Node, targetSum int) bool {
+	if node == nil {
+		return false
+	}
+
+	if (node.left == nil) && (node.right == nil) && (node.value == targetSum) {
+		return true
+	}
+
+	targetSum -= node.value
+
+	return pathSum(node.left, targetSum) || pathSum(node.right, targetSum)
+
+}
+
+func (bst *binarySearchTree) InvertTree() {
+	invertTree(bst.root)
+}
+
+func invertTree(node *Node) *Node {
+	if node != nil {
+		node.left, node.right = invertTree(node.right), invertTree(node.left)
+		return node
+	}
+	return node
+}
+
+func (bst *binarySearchTree) SumOfRightLeaves() int {
+	return sumOfRightLeaves(bst.root)
+}
+
+func sumOfRightLeaves(node *Node) int {
+	if node == nil {
+		return 0
+	}
+	value := 0
+	if (node.right != nil) && (node.right.right == nil) && (node.right.left == nil) {
+		value += node.right.value
+	}
+
+	return value + sumOfRightLeaves(node.left) + sumOfRightLeaves(node.right)
+}
+
+func (bst *binarySearchTree) SumOfLeftLeaves() int {
+	return sumOfLeftLeaves(bst.root)
+}
+
+func sumOfLeftLeaves(node *Node) int {
+	if node == nil {
+		return 0
+	}
+
+	value := 0
+	if (node.left != nil) && (node.left.left == nil) && (node.left.right == nil) {
+		value += node.left.value
+	}
+
+	return value + sumOfLeftLeaves(node.left) + sumOfLeftLeaves(node.right)
+
+}
+
+func (bst *binarySearchTree) AllElements() []int {
+	elements := new([]int)
+	allElements(bst.root, elements)
+
+	return *elements
+}
+
+func allElements(node *Node, elements *[]int) {
+	if node == nil {
+		return
+	}
+
+	*elements = append(*elements, node.value)
+
+	allElements(node.left, elements)
+	allElements(node.right, elements)
 }
--- binary_search_tree/binarySearchTree_test.go
+++ binary_search_tree/binarySearchTree_test.go
@@ -28,3 +28,251 @@ func TestBinarySearchTree_Insert(t *testing.T) {
 	// Watch values
 	bst.Inorder()
 }
+
+func TestBinarySearchTree_Search(t *testing.T) {
+	// Init
+	bst := NewBinarySearchTree()
+
+	bst.Insert(6)
+	bst.Insert(4)
+	bst.Insert(9)
+	bst.Insert(7)
+	bst.Insert(2)
+	bst.Insert(10)
+
+	// Execution and Test
+	assert.True(t, bst.Search(6))
+	assert.True(t, bst.Search(2))
+	assert.False(t, bst.Search(3))
+
+	// Watch values
+	bst.Inorder()
+}
+
+// 先頭を削除する場合のテスト
+func TestBinarySearchTree_Remove(t *testing.T) {
+	// Init
+	bst := NewBinarySearchTree()
+
+	bst.Insert(6)
+	bst.Insert(4)
+	bst.Insert(9)
+	bst.Insert(7)
+	bst.Insert(2)
+	bst.Insert(10)
+
+	// Execution
+	bst.Remove(6)
+
+	// Test
+	assert.Equal(t, 7, bst.root.value)
+	assert.Equal(t, 9, bst.root.right.value)
+	assert.Equal(t, 4, bst.root.left.value)
+	assert.Equal(t, 2, bst.root.left.left.value)
+	assert.Equal(t, 10, bst.root.right.right.value)
+
+	// Watch values
+	bst.Inorder()
+}
+
+// 子コードが左右あるノードの削除
+func TestBinarySearchTree_Remove2(t *testing.T) {
+	// Init
+	bst := NewBinarySearchTree()
+
+	bst.Insert(6)
+	bst.Insert(4)
+	bst.Insert(9)
+	bst.Insert(7)
+	bst.Insert(2)
+	bst.Insert(10)
+
+	// Execution
+	bst.Remove(9)
+
+	// Test
+	assert.Equal(t, 6, bst.root.value)
+	assert.Equal(t, 10, bst.root.right.value)
+	assert.Equal(t, 4, bst.root.left.value)
+	assert.Equal(t, 2, bst.root.left.left.value)
+	assert.Equal(t, 7, bst.root.right.left.value)
+
+	// Watch values
+	bst.Inorder()
+}
+
+// 片方子ノードがあるノードを削除する場合
+func TestBinarySearchTree_Remove3(t *testing.T) {
+	// Init
+	bst := NewBinarySearchTree()
+
+	bst.Insert(6)
+	bst.Insert(4)
+	bst.Insert(9)
+	bst.Insert(7)
+	bst.Insert(2)
+	bst.Insert(10)
+
+	// Execution
+	bst.Remove(4)
+
+	// Test
+	assert.Equal(t, 6, bst.root.value)
+	assert.Equal(t, 9, bst.root.right.value)
+	assert.Equal(t, 2, bst.root.left.value)
+	assert.Equal(t, 7, bst.root.right.left.value)
+	assert.Equal(t, 10, bst.root.right.right.value)
+
+	// Watch values
+	bst.Inorder()
+}
+
+// 子ノード無しのノード削除する場合
+func TestBinarySearchTree_Remove4t(t *testing.T) {
+	// Init
+	bst := NewBinarySearchTree()
+
+	bst.Insert(6)
+	bst.Insert(4)
+	bst.Insert(9)
+	bst.Insert(7)
+	bst.Insert(2)
+	bst.Insert(10)
+
+	// Execution
+	bst.Remove(7)
+
+	// Test
+	assert.Equal(t, 6, bst.root.value)
+	assert.Equal(t, 9, bst.root.right.value)
+	assert.Equal(t, 4, bst.root.left.value)
+	assert.Equal(t, 2, bst.root.left.left.value)
+	assert.Equal(t, 10, bst.root.right.right.value)
+
+	// Watch values
+	bst.Inorder()
+}
+
+func TestBinarySearchTree_MaxDepth(t *testing.T) {
+	// Init
+	bst := NewBinarySearchTree()
+
+	bst.Insert(6)
+	bst.Insert(4)
+	bst.Insert(9)
+	bst.Insert(7)
+	bst.Insert(2)
+	bst.Insert(10)
+
+	// Execution and Test
+	assert.Equal(t, 3, bst.MaxDepth())
+
+	// Watch values
+	bst.Inorder()
+}
+
+func TestBinarySearchTree_PathSum(t *testing.T) {
+	// Init
+	bst := NewBinarySearchTree()
+
+	bst.Insert(6)
+	bst.Insert(4)
+	bst.Insert(9)
+	bst.Insert(7)
+	bst.Insert(2)
+	bst.Insert(10)
+
+	// Execution and Test
+	assert.True(t, bst.PathSum(12))
+	assert.True(t, bst.PathSum(22))
+	assert.True(t, bst.PathSum(25))
+
+	assert.False(t, bst.PathSum(10))
+	assert.False(t, bst.PathSum(38))
+
+	// Watch values
+	bst.Inorder()
+
+}
+func TestBinarySearchTree_InvertTree(t *testing.T) {
+	// Init
+	bst := NewBinarySearchTree()
+
+	bst.Insert(6)
+	bst.Insert(4)
+	bst.Insert(9)
+	bst.Insert(7)
+	bst.Insert(2)
+	bst.Insert(10)
+
+	// Execution
+	bst.InvertTree()
+
+	// Test
+	assert.Equal(t, 6, bst.root.value)
+	assert.Equal(t, 4, bst.root.right.value)
+	assert.Equal(t, 9, bst.root.left.value)
+	assert.Equal(t, 10, bst.root.left.left.value)
+	assert.Equal(t, 2, bst.root.right.right.value)
+	assert.Equal(t, 7, bst.root.left.right.value)
+
+	// Watch values
+	bst.Inorder()
+}
+
+func TestBinarySearchTree_SumOfRightLeaves(t *testing.T) {
+	// Init
+	bst := NewBinarySearchTree()
+
+	bst.Insert(6)
+	bst.Insert(4)
+	bst.Insert(9)
+	bst.Insert(7)
+	bst.Insert(2)
+	bst.Insert(10)
+
+	// Execution and Test
+	assert.Equal(t, 10, bst.SumOfRightLeaves())
+
+}
+
+func TestBinarySearchTree_SumOfLeftLeaves(t *testing.T) {
+	// Init
+	bst := NewBinarySearchTree()
+
+	bst.Insert(6)
+	bst.Insert(4)
+	bst.Insert(9)
+	bst.Insert(7)
+	bst.Insert(2)
+	bst.Insert(10)
+
+	// Execution and Test
+	assert.Equal(t, 9, bst.SumOfLeftLeaves())
+}
+
+func TestBinarySearchTree_AllTreePaths(t *testing.T) {
+	// Init
+	bst := NewBinarySearchTree()
+
+	bst.Insert(6)
+	bst.Insert(4)
+	bst.Insert(9)
+	bst.Insert(7)
+	bst.Insert(2)
+	bst.Insert(10)
+
+	// Execution
+	got := bst.AllElements()
+
+	// Test
+	for i, value := range got {
+		if value != got[i] {
+			t.Fail()
+		}
+	}
+
+	if len(got) != 6 {
+		t.Fail()
+	}
+}
